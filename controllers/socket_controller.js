891b35272396abc4f2d9255aaf22caef25031d52
marialovgren <marialovgren86@hotmail.com>
2022-05-23 10:44:17 +0200

    testing diffent ways of saving players to player array

--- controllers/socket_controller.js
+++ controllers/socket_controller.js
@@ -5,16 +5,27 @@
 const debug = require('debug')('battleship:socket_controller');
 let io = null; 
 
-const players = []
+const players = []  
 
-/* 
-const game = [
+/* const games = [
 	{
 		id: 1,
-		name: "game",
+		name: "Game",
 		players: {},
 	}
-] */
+]  */
+
+//******** GET GAME BY ID ********//
+
+/* const getGameById = id => {
+	return games.find(game => game.id === id)
+} */
+
+//******** GET GAME BY PLAYER ID ********//
+
+/* const getGameByPlayerId = id => {
+	return games.find(gameroom => gameroom.players.hasOwnProperty(id))
+} */
 
 
 //******** PLAYER JOINS GAME ********//
@@ -22,26 +33,69 @@ const game = [
  const handleJoinGame = function(username) {
 	debug(`Player ${username} with socket id ${this.id} wants to join the game`);
 
-	// Check if there are one or two players connected
-	if (players.length <= 1) {
-		// Create a player with socket id and username
-		const player = {
+	/* // join game
+	this.join(game_id)
+
+	// add socket to list of players in this game
+	// 1. find game
+	const game = getGameById(game_id)
+
+	// 2. add socket to game´s 'players' object
+	game.players[this.id] = username
+
+	//let everyone know that someone has joined the game
+	this.broadcast.to(game.id).emoit('player:joined', username)
+
+	// confirm join
+	callback({
+		success: true,
+		gameName: game.name,
+		players: game.players
+	})
+
+	// updater list of players
+	io.to(game.id).emit('update:players', game.players) */
+
+	// If there are no player connected, then the first player will be playerOne
+	if (players.length === 0) { 
+		// Create playerOne with socket id and username
+		const playerOne = [{
 			socket_id: this.id,
 			username: username,
 			game: "game",
 			currentPlayer: "", 
+		}]
+
+
+		this.join(playerOne.game)
+
+		// Add the connected player to the arrays of players  
+		players.push(playerOne)
+		console.log(`PlayerOne: ${playerOne}, Players: ${players}`)
+		debug(`PlayerOne: ${playerOne}, Players: ${players}`)
+
+		// update list of players
+		io.to(playerOne.game).emit('update:players', players) 
+		debug(`Playerlist: ${players}`)
+	
+ 	} else if (players.length <= 1) {
+		 //if the connected player is the second player then save details to playerTwo
+		const playerTwo = {
+			socket_id: this.id,
+			username: username,
+			game: "game",
 		}
 
-		this.join(player.game)
+		this.join(playerTwo.game)
 
-		// Add the connected player to the player array 
-		players.push(player)
-		console.log("Players before emit", players)
+		players.push(playerTwo)
 
-		// updater list of players
-		io.to(player.game).emit('update:players', players)
+		console.log(`PlayerTwo: ${playerTwo}, Players: ${players}`)
+		debug(`PlayerTwo: ${playerTwo}, Players: ${players}`)
 
-	} else {	
+		// update list of players
+		io.to(playerTwo.game).emit('update:players', players)
+	 } else {	
 		console.log("Game is full")
 		debug(`Game is full. Connected players are ${username}`);
 		this.emit('game:full', true, (players) => {
@@ -57,7 +111,7 @@ const game = [
  const handleDisconnect = function() {
 	debug(`Client ${this.id} disconnected :(`);
 
-	const removePlayer = (id) => {	
+	/* const removePlayer = (id) => {	
 		// find player that disconnected
 		const removePlayerIndex = players.findIndex((player) => player.id === id)
 		if (removePlayerIndex !== -1) 
@@ -66,7 +120,7 @@ const game = [
 	}
 
 	const player = removePlayer(this.id)
-	if (player) io.to(player.game).emit('player:disconnected', true)
+	if (player) io.to(player.game).emit('player:disconnected', true) */
 }
 
 /**
